1376ee68fdab34a1cdfc1d9bd202dcbcd553cd30
Francisco Javier DLG <pacodiazl@outlook.com>
2023-08-09 18:34:25 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Css/Menu.css';
-import KRouter from './Router';
+import KRouter from './router';
 import 'react-toastify/dist/ReactToastify.css';
 import {ToastContainer} from 'react-toastify';
 
