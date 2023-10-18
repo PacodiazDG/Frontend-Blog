4e2d6bc4a77fbac517ca42fedcd4bbd68255041c
Francisco Javier DLG <pacodiazl@outlook.com>
2023-10-18 13:25:19 -0600

    Update Gui.ts

--- src/Logic/Gui.ts
+++ src/Logic/Gui.ts
@@ -1,4 +1,4 @@
 export default function randEmoji() {
-  const emoji = [':‑(', ':‑<', ':‑[', ':-||', '>:[', ':(', ':\'(', '</3', ':\'v'];
+  const emoji = [':‑(', ':‑<', ':‑[', ':-||', '>:[', ':(', ':\'(', '</3'];
   return emoji[Math.floor(Math.random() * emoji.length)];
 }
