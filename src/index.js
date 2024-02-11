233439f22522beb72c315861599555f600683bcc
Francisco Javier DLG <pacodiazl@outlook.com>
2024-02-11 01:49:42 -0600

    change  ReactDOM.render  to   createRoot

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './Default.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap';
 
-ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root'),
-);
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
